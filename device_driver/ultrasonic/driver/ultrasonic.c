263750943ac576c81318e33d591169a3bed32726
grapegun67 <57746289+grapegun67@users.noreply.github.com>
2023-04-21 14:58:13 +0900

    Update ultrasonic.c

--- device_driver/ultrasonic/driver/ultrasonic.c
+++ device_driver/ultrasonic/driver/ultrasonic.c
@@ -36,14 +36,14 @@ static int sonic_release(struct inode *inode, struct file *file)
         return 0;
 }
 
-/* /sys/class/hc_sr04/value 파일의 내용을 읽어 들임 - TRIG pin을 설정하여 초음파 소리 전달 시작하도록 만듦	*/
+/* /sys/class/hc_sr04/value 파일 read를 통해 TRIG pin을 설정하여 초음파 소리 전달 시작	*/
 static ssize_t hc_sr04_value_show(struct file *filp, char __user *buf, size_t len, loff_t *off)
 {
 	int counter;
 
 	gpio_set_value(HC_SR04_TRIG_PIN, GPIO_HIGH);
 
-	/* datasheet에 의하면 최소 10 us 동안 Trig pin이 HIGH 상태로 유지되어야 함 */
+	/* datasheet에 따라 최소 10us 동안 Trig pin이 HIGH 상태로 유지 필요 */
 	udelay(15);   
 
 	gpio_set_value(HC_SR04_TRIG_PIN, GPIO_LOW);
@@ -55,7 +55,7 @@ static ssize_t hc_sr04_value_show(struct file *filp, char __user *buf, size_t le
 		if (++counter > 2320000)
 		{
 			pr_info(KERN_INFO "debug0 [%d]\n", counter);
-			return sprintf(buf, "[%d] [%d]\n", counter,  -1);/* copy_to_user() 사용해야할지도	*/
+			return sprintf(buf, "[%d] [%d]\n", counter,  -1);/* copy_to_user() 사용 고려 필요	*/
 		}
 
 		udelay(1);
@@ -65,7 +65,7 @@ static ssize_t hc_sr04_value_show(struct file *filp, char __user *buf, size_t le
 	return sprintf(buf, "%lld\n", ktime_to_us(ktime_sub(echo_stop, echo_start)));  
 }
 
-/* /sys/class/hc_sr04/value 파일에 값을 씀 - 실제로는 하는 일 없음	*/
+/* /sys/class/hc_sr04/value 파일 write(실제로는 하는 일 없음)	*/
 static ssize_t hc_sr04_value_store(struct file *filp, const char __user *buf, size_t len, loff_t *off)
 {
 	pr_info(KERN_INFO "Buffer len %d bytes\n", len);
@@ -85,7 +85,7 @@ static struct file_operations fops =
         .release        = sonic_release,
 };
 
-/* echo pin 관련 interrupt handler 함수 - ECHO pin이 HIGH일 때의 시간과 LOW일 때의 시간을 구함, 둘의 차이 값을 거리 계산시 사용함 */
+/* echo pin 관련 interrupt handler 함수 - ECHO pin이 HIGH일 때의 시간과 LOW일 때의 시간을 구한 뒤, 차이값으로 거리 측정 */
 static irqreturn_t echo_interrupt_handler(int irq, void *data)
 {
 	ktime_t time_value;
@@ -104,7 +104,6 @@ static irqreturn_t echo_interrupt_handler(int irq, void *data)
 			echo_stop = time_value;
 			echo_valid_flag = ECHO_VALID;
 		}
-    
 	}
 
 	return IRQ_HANDLED;
@@ -112,35 +111,35 @@ static irqreturn_t echo_interrupt_handler(int irq, void *data)
 
 static int __init hc_sr04_init(void)
 {
-  int ret;
+	int ret;
   
-  if((alloc_chrdev_region(&dev, 0, 1, "sonic_dev")) < 0)
+	if((alloc_chrdev_region(&dev, 0, 1, "sonic_dev")) < 0)
 	{
-    pr_err("Error: cannot allocate major number\n");
+		pr_err("Error: cannot allocate major number\n");
 		goto error;
-  }
+	}
   
-  pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+	pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
 
-  cdev_init(&sonic_cdev, &fops);
+	cdev_init(&sonic_cdev, &fops);
 
-  if((cdev_add(&sonic_cdev, dev, 1)) < 0)
+	if((cdev_add(&sonic_cdev, dev, 1)) < 0)
 	{
-    pr_err("Error: annot add device \n");
+		pr_err("Error: annot add device \n");
 		goto error;
-  }
+	}
 
-  if((dev_class = class_create(THIS_MODULE, "sonic_class")) == NULL)
+	if((dev_class = class_create(THIS_MODULE, "sonic_class")) == NULL)
 	{
-    pr_err("Error: cannot create the struct class\n");
+		pr_err("Error: cannot create the struct class\n");
 		goto error;
-  }
+	}
 
-  if((device_create(dev_class, NULL, dev, NULL, "sonic_device")) == NULL)
+	if((device_create(dev_class, NULL, dev, NULL, "sonic_device")) == NULL)
 	{
-    pr_err( "Error: cannot create device \n");
+		pr_err( "Error: cannot create device \n");
 		goto error;
-  }
+	}
 
 	ret = gpio_request_one(HC_SR04_TRIG_PIN, GPIOF_DIR_OUT, "TRIG");  /* TRIG pin GPIO output 사용 요청 */
 	if (ret < 0)
@@ -164,17 +163,17 @@ static int __init hc_sr04_init(void)
 	}
 
 	else
-    gpio_irq = ret;
-		                  		    /* interrupt handler 등록 */  	/* interrupt 발생 조건 */
+		gpio_irq = ret;
+	
+	/* echo_interrupt_handler: interrupt handler등록, IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING: 엣지 트리거 방식으로 인터럽트 처리	*/
 	ret = request_irq(gpio_irq, echo_interrupt_handler, IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING, "hc-sr04-echo", NULL);
-	pr_info(KERN_ERR "request IRQ(%d)\n", ret);
 	if (ret)
 	{
 		pr_info(KERN_ERR "Failed to request IRQ(%d)\n", ret);
 		goto error;
 	}
 
-  return 0;
+	return 0;
 
 error:
     return -1;
@@ -184,19 +183,19 @@ static void __exit hc_sr04_exit(void)
 {
 	/* interrupt 등록 해제 */
 	if (gpio_irq != -1)
-    free_irq(gpio_irq, NULL);  
+		free_irq(gpio_irq, NULL);  
 
- 	gpio_unexport(HC_SR04_TRIG_PIN);
-  gpio_unexport(HC_SR04_ECHO_PIN);
-
+	gpio_unexport(HC_SR04_TRIG_PIN);
+	gpio_unexport(HC_SR04_ECHO_PIN);
 	gpio_free(HC_SR04_TRIG_PIN);	/* GPIO 설정 해제 */
 	gpio_free(HC_SR04_ECHO_PIN);	/* GPIO 설정 해제 */
-  device_destroy(dev_class,dev);
-  class_destroy(dev_class);
-  cdev_del(&sonic_cdev);
-  unregister_chrdev_region(dev, 1);
+	
+	device_destroy(dev_class,dev);
+	class_destroy(dev_class);
+	cdev_del(&sonic_cdev);
+	unregister_chrdev_region(dev, 1);
 
-  pr_info("GPIO driver done\n");
+	pr_info("GPIO driver done\n");
 }
 
 module_init(hc_sr04_init);
